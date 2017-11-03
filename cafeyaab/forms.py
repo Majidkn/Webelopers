916a44f34ebfa69f6fd790d801e9b6a983d5cd39
Majid Nayyeri <majidrobo@gmail.com>
2017-11-04 00:17:15 +0330

    update edit profile page's style

--- cafeyaab/forms.py
+++ cafeyaab/forms.py
@@ -9,7 +9,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from captcha.fields import CaptchaField
 
-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from cafeyaab.models import User
@@ -20,14 +19,13 @@ from django.forms.models import ModelForm
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='Required.', label='Name')
     email = forms.EmailField(max_length=254, help_text='Required.')
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput,)
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput, )
     password2 = forms.CharField(label="Password", widget=forms.PasswordInput, )
     captcha = CaptchaField()
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email', 'password1', 'password2', 'captcha')
-
 
 
 class EditForm(ModelForm):
